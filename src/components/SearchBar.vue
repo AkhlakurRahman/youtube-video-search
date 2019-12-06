39f22f7272ccabacb4ab3c6682c03b1266a97edf
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-12-07 02:55:49 +0600

    initial commit

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<input type="search" @input="onInput" />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SearchBar',
+	methods: {
+		onInput: function(event) {
+			this.$emit('searchInput', event.target.value);
+		}
+	}
+};
+</script>
+
+<style scoped></style>
